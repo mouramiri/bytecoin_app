b0657d65382417cda76c6d3c501f0b5729ccf3aa
Miriana Moura <mouramiri@outlook.com>
2020-03-09 00:18:33 -0400

    Add files via upload

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Miriana Moura on 3/9/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
